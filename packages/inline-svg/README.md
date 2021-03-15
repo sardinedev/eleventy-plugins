c4e0cc074e3abd883b791b29d4dfbb4185b3165e
Hugo Nogueira <hnogueira@marabyte.com>
2021-03-15 23:47:58 +0000

    chore: initial code

---
+++ packages/inline-svg/README.md
@@ -0,0 +1,30 @@
+# `@sardine/eleventy-plugin-inline-svg`
+
+An 11ty plugin to inline SVG.
+
+## Features
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/download/) 12 and up
+
+- [11ty](https://www.11ty.dev/) 0.11
+
+## Installation
+
+```bash
+npm install --save-dev @sardine/eleventy-plugin-inline-svg
+```
+
+## How to use it
+
+```javascript
+const safeLinks = require('@sardine/eleventy-plugin-external-links');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(safeLinks);
+};
+```
+
+## License
+
+[MIT](./LICENSE)
---
+++ packages/inline-svg/lib/.eleventy.ts
@@ -0,0 +1,58 @@
+const path = require('path');
+const fs = require('fs');
+const { optimize } = require('svgo');
+
+const config = {
+  plugins: [
+    'removeDoctype',
+    'removeXMLProcInst',
+    'removeComments',
+    'removeMetadata',
+    'removeXMLNS',
+    'removeEditorsNSData',
+    'cleanupAttrs',
+    'inlineStyles',
+    'minifyStyles',
+    'convertStyleToAttrs',
+    'cleanupIDs',
+    'removeRasterImages',
+    'removeUselessDefs',
+    'cleanupNumericValues',
+    'cleanupListOfValues',
+    'convertColors',
+    'removeUnknownsAndDefaults',
+    'removeNonInheritableGroupAttrs',
+    'removeUselessStrokeAndFill',
+    'removeViewBox',
+    'cleanupEnableBackground',
+    'removeHiddenElems',
+    'removeEmptyText',
+    'convertShapeToPath',
+    'moveElemsAttrsToGroup',
+    'moveGroupAttrsToElems',
+    'collapseGroups',
+    'convertPathData',
+    'convertEllipseToCircle',
+    'convertTransform',
+    'removeEmptyAttrs',
+    'removeEmptyContainers',
+    'mergePaths',
+    'removeUnusedNS',
+    'reusePaths',
+    'sortAttrs',
+    'sortDefsChildren',
+    'removeTitle',
+    'removeDesc',
+    'removeDimensions',
+    'removeStyleElement',
+    'removeScriptElement',
+  ],
+};
+
+export default (eleventyConfig: any, options) => {
+  eleventyConfig.addAsyncShortcode('sardine-inline-svg', async (svgName: string) => {
+    const svgData: string = fs.readFileSync(path.join(options.baseUrl, `${svgName}.svg`), 'utf8');
+    const { data } = await optimize(svgData, { ...config });
+    return data;
+  });
+};
