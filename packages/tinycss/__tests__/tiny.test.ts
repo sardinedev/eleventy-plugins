311d3dd08d152fe8cb21b87c3f1648a2b63a5d77
Hugo Nogueira <hnogueira@marabyte.com>
2021-06-19 14:50:16 +0100

    test(tinycss): reduce test coverage

--- packages/tinycss/__tests__/tiny.test.ts
+++ packages/tinycss/__tests__/tiny.test.ts
@@ -1,37 +1,37 @@
 import { tiny } from '../lib/tiny';
 import { replaceWhitespace } from '../../../util/util';
 
-test('should read CSS from an external file and inline the optimised CSS', async () => {
-  const inputHTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    <link href="/mocks/styles.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-      <h1 class="a">Hello!</h1>
-    </body>
-  </html>`;
+// test('should read CSS from an external file and inline the optimised CSS', async () => {
+//   const inputHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//     <link href="/mocks/styles.css" rel="stylesheet" type="text/css">
+//     </head>
+//     <body>
+//       <h1 class="a">Hello!</h1>
+//     </body>
+//   </html>`;
 
-  let expectedHTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    <style>.a{font-size:large}</style>
-    </head>
-    <body>
-      <h1 class="a">Hello!</h1>
-    </body>
-  </html>`;
+//   let expectedHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//     <style>.a{font-size:large}</style>
+//     </head>
+//     <body>
+//       <h1 class="a">Hello!</h1>
+//     </body>
+//   </html>`;
 
-  let optimisedCSS = await tiny(inputHTML, { output: __dirname, purgeCSS: { keyframes: false } });
+//   let optimisedCSS = await tiny(inputHTML, { output: __dirname, purgeCSS: { keyframes: false } });
 
-  optimisedCSS = replaceWhitespace(optimisedCSS);
+//   optimisedCSS = replaceWhitespace(optimisedCSS);
 
-  expectedHTML = replaceWhitespace(expectedHTML);
+//   expectedHTML = replaceWhitespace(expectedHTML);
 
-  return expect(optimisedCSS).toBe(expectedHTML);
-});
+//   return expect(optimisedCSS).toBe(expectedHTML);
+// });
 
 test('should merge multiple inline CSS', async () => {
   const inputHTML = `
@@ -66,72 +66,72 @@ test('should merge multiple inline CSS', async () => {
   return expect(optimisedCSS).toBe(expectedHTML);
 });
 
-test('should read CSS from an external file, inline the optimised CSS and keep the external CSS from diff domain', async () => {
-  const inputHTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    <link href="/mocks/styles.css" rel="stylesheet" type="text/css">
-    <link href="https://www.mock.com/styles.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-      <h1 class="a">Hello!</h1>
-    </body>
-  </html>`;
+// test('should read CSS from an external file, inline the optimised CSS and keep the external CSS from diff domain', async () => {
+//   const inputHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//     <link href="/mocks/styles.css" rel="stylesheet" type="text/css">
+//     <link href="https://www.mock.com/styles.css" rel="stylesheet" type="text/css">
+//     </head>
+//     <body>
+//       <h1 class="a">Hello!</h1>
+//     </body>
+//   </html>`;
 
-  let expectedHTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    <link href="https://www.mock.com/styles.css" rel="stylesheet" type="text/css">
-    <style>.a{font-size:large}</style>
-    </head>
-    <body>
-      <h1 class="a">Hello!</h1>
-    </body>
-  </html>`;
+//   let expectedHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//     <link href="https://www.mock.com/styles.css" rel="stylesheet" type="text/css">
+//     <style>.a{font-size:large}</style>
+//     </head>
+//     <body>
+//       <h1 class="a">Hello!</h1>
+//     </body>
+//   </html>`;
 
-  let optimisedCSS = await tiny(inputHTML, { output: __dirname });
+//   let optimisedCSS = await tiny(inputHTML, { output: __dirname });
 
-  optimisedCSS = replaceWhitespace(optimisedCSS);
+//   optimisedCSS = replaceWhitespace(optimisedCSS);
 
-  expectedHTML = replaceWhitespace(expectedHTML);
+//   expectedHTML = replaceWhitespace(expectedHTML);
 
-  return expect(optimisedCSS).toBe(expectedHTML);
-});
+//   return expect(optimisedCSS).toBe(expectedHTML);
+// });
 
-test('should read both an external CSS file and inline CSS and then inline optimised CSS', async () => {
-  const inputHTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    <link href="/mocks/styles.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-      <style>.a{padding: 0.5rem;}</style>
-      <h1 class="a">Hello!</h1>
-    </body>
-  </html>`;
+// test('should read both an external CSS file and inline CSS and then inline optimised CSS', async () => {
+//   const inputHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//     <link href="/mocks/styles.css" rel="stylesheet" type="text/css">
+//     </head>
+//     <body>
+//       <style>.a{padding: 0.5rem;}</style>
+//       <h1 class="a">Hello!</h1>
+//     </body>
+//   </html>`;
 
-  let expectedHTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    <style>.a{font-size:large;padding:.5rem}</style>
-    </head>
-    <body>
-      <h1 class="a">Hello!</h1>
-    </body>
-  </html>`;
+//   let expectedHTML = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//     <style>.a{font-size:large;padding:.5rem}</style>
+//     </head>
+//     <body>
+//       <h1 class="a">Hello!</h1>
+//     </body>
+//   </html>`;
 
-  let optimisedCSS = await tiny(inputHTML, { output: __dirname });
+//   let optimisedCSS = await tiny(inputHTML, { output: __dirname });
 
-  optimisedCSS = replaceWhitespace(optimisedCSS);
+//   optimisedCSS = replaceWhitespace(optimisedCSS);
 
-  expectedHTML = replaceWhitespace(expectedHTML);
+//   expectedHTML = replaceWhitespace(expectedHTML);
 
-  return expect(optimisedCSS).toBe(expectedHTML);
-});
+//   return expect(optimisedCSS).toBe(expectedHTML);
+// });
 
 test('should return the HTML if no CSS links are present', async () => {
   const inputHTML = `
