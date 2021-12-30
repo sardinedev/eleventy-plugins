1748582b8cdaf140be96ca58a58654592ba34120
Hugo Nogueira <hnogueira@marabyte.com>
2021-12-30 15:17:36 +0000

    chore: update code infra to use turborepo (#39)
    * chore: replace Lerna with Turborepo
    
    * chore: replace Microbuild with Parcel to build packages
    
    * chore: add SWC to Jest
    
    * chore: update test runner
    
    * chore: add @changesets
    
    * chore: remove codecov package
    
    * breaking: drop node 12 support
    
    * chore: update codecov action to v2

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
   // collectCoverageFrom: null,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -129,7 +129,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node"
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -164,7 +164,21 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: null,
+  transform: {
+    '^.+\\.ts?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: false,
+            decorators: false,
+            dynamicImport: false,
+          },
+        },
+      },
+    ],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
