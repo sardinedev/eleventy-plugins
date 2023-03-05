7517b2c6367898fb4ea49a20badf32a0178e752b
Hugo Nogueira <hnogueira@marabyte.com>
2023-03-05 19:23:36 +0000

    test

--- packages/code-highlighter/lib/.eleventy.ts
+++ packages/code-highlighter/lib/.eleventy.ts
@@ -1,20 +1,20 @@
 import { highlighter } from './highlighter';
 
 export interface OptionsInterface {
-  urlTheme?: string;
+	urlTheme?: string;
 }
 
 export default (eleventyConfig: any, options?: OptionsInterface) => {
-  eleventyConfig.namespace('sardine-', () => {
-    eleventyConfig.addTransform('code-highlighter', async (content: string, outputPath: string) => {
-      try {
-        if (outputPath && outputPath.endsWith('.html')) {
-          content = await highlighter(content, options?.urlTheme);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-      return content;
-    });
-  });
+	eleventyConfig.namespace('sardine-', () => {
+		eleventyConfig.addTransform('code-highlighter', async (content: string, outputPath: string) => {
+			try {
+				if (outputPath?.endsWith('.html')) {
+					content = await highlighter(content, options?.urlTheme);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+			return content;
+		});
+	});
 };
